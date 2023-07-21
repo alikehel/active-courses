950866000085d2e4e3019a780e5a66eb61f27adc
Ali Kehel <ali.saber.27.2@gmail.com>
2023-07-21 09:20:00 +0300

    test arabic

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import React from 'react';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>My Project</span>,
+	project: {
+		link: 'https://github.com/shuding/nextra-docs-template',
+	},
+	chat: {
+		link: 'https://discord.com',
+	},
+	docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+	footer: {
+		text: 'Nextra Docs Template',
+	},
+	i18n: [
+		{ locale: 'en', text: 'English' },
+		{ locale: 'ar', text: 'العربية', direction: 'rtl' },
+	],
+};
 
-export default config
+export default config;
