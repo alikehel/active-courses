dce277f22b17efd7ea6bc063c2e81258a1171d53
Ali Kehel <ali.saber.27.2@gmail.com>
2023-07-23 15:48:04 +0300

    add rules, faq, etc...

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,21 +2,30 @@ import { DocsThemeConfig } from 'nextra-theme-docs';
 import React from 'react';
 
 const config: DocsThemeConfig = {
-	logo: <span>My Project</span>,
+	logo: (
+		<div style={{ alignItems: 'center', display: 'flex', gap: '8px' }}>
+			<img width="30" height="30" src="https://cdn.discordapp.com/icons/950993900720492574/d56dcdcc7d1baf26a39ff6f8e0900478.webp?size=96" alt="" />
+			<span style={{ fontWeight: 'bold', fontSize: 18 }}>Active Courses</span>
+		</div>
+	),
+
 	project: {
-		link: 'https://github.com/shuding/nextra-docs-template',
+		link: 'https://github.com/alisaber272/active-courses',
 	},
 	chat: {
-		link: 'https://discord.com',
+		link: 'https://discord.gg/QrfTN2Aukx',
 	},
-	docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+	docsRepositoryBase: 'https://github.com/alisaber272/active-courses/tree/main',
 	footer: {
-		text: 'Nextra Docs Template',
+		text: 'Active Courses © 2023',
 	},
 	i18n: [
 		{ locale: 'en', text: 'English' },
 		{ locale: 'ar', text: 'العربية', direction: 'rtl' },
 	],
+	search: {
+		placeholder: 'Search',
+	},
 };
 
 export default config;
